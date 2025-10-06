dc7de8c440684772e2ed14268cd4a52d6781ce70
NourSaber <nour01saber@gmail.com>
2025-10-06 14:54:56 +0300

    Create Department Impelementation

--- Demo.PL/Controllers/DepartmentsController.cs
+++ Demo.PL/Controllers/DepartmentsController.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Demo.BLL.Services;
+using Demo.BLL.DTOs;
+
 namespace Demo.PL.Controllers
 {
-    public class DepartmentsController(IDepartmentService _departmentService) : Controller
+    public class DepartmentsController(IDepartmentService _departmentService , ILogger<HomeController> logger,IWebHostEnvironment _environment) : Controller
     {
         // DepartmentService departmentService used Across All Actions
         // EmployeeService --> Assign Manager : this Service Needed Only For One Action
@@ -18,5 +20,55 @@ namespace Demo.PL.Controllers
             var departments = _departmentService.GetAllDepartments();
             return View(departments); 
         }
+
+        #region Create
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreatedDepartmentDto departmentDto)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    int res = _departmentService.AddDepartment(departmentDto);
+                    if(res > 0)
+                    {
+                        return RedirectToAction(nameof(Index));
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, "Department Cannot be added");
+                        return View(departmentDto);
+                    }
+                }
+                catch (Exception ex) {
+                    // Log Exception
+                    if (_environment.IsDevelopment())
+                    {
+                        //1)Development => log error in Console And Return The Same View with error message 
+                        ModelState.AddModelError(string.Empty, ex.Message);
+                        return View(departmentDto);
+                    }else
+                    {
+                        //2)Deployment => log errors In File | Table And Return the same view with error message
+                        //logger.LogError(ex.Message);
+                        return View(departmentDto);
+
+
+                    }
+                }
+
+            }
+            else return View(departmentDto);
+        }
+
+
+        #endregion
     }
 }
